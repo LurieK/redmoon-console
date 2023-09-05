fa7d642029f4b0525f5e67d5c4ee4d96b4cfeff1
LurieK <LuraineKimmerle@gmail.com>
2023-09-05 17:48:41 +0800

    create feilds for contact form set state to handle inputs

--- redmoon-sign-up/src/components/contact-form.js
+++ redmoon-sign-up/src/components/contact-form.js
@@ -1,15 +1,35 @@
+import React from 'react'
 
 const ContactForm = () => {
+  const [formData, setFormData]= React.useState({
+    company: '',
+    name: '',
+    email: '',
+    phone: '',
+    username: '',
+    password: '',
+    confPassword: ''
+
+  })
+
     const handleSubmit = (e) => {
       e.preventDefault();
       // Handle form submission, save data, etc.
     };
   
     return (
-      <form onSubmit={handleSubmit}>
+      <form className='sign-up-form' onSubmit={handleSubmit}>
+        <input type="text" name="company" placeholder="Company" />
         <input type="text" name="name" placeholder="Name" />
         <input type="email" name="email" placeholder="Email" />
-        <button type="submit">Next</button>
+        <input type="text" name="phone" placeholder="Phone" />
+        <input type="text" name="user-name" placeholder="User Name" />
+        <input type="text" name="password" placeholder="Password" />
+        <p>Must be more then 8 characters and include at least 1 number and 1 symbole</p>
+        <input type="text" name="confirm-password" placeholder="Confirm Password" />
+        <input type="checkbox" name="newsletter" />
+        <label htmlFor="newsletter">Keep me up to date with the latest news</label>
+        <button type="submit">Continue to Payment</button>
       </form>
     );
   };
