e89af1d2240e599002afdcc31ad4f103622a2e53
LurieK <LuraineKimmerle@gmail.com>
2023-09-06 09:19:45 +0800

    When testing to view the payment form I am getting errors
    the problem seems to be  stripe not being initialized before Elements needs it
    
    trying a useEffect and useState with async/await function

--- redmoon-sign-up/src/components/stripeParent.js
+++ redmoon-sign-up/src/components/stripeParent.js
@@ -1,12 +1,22 @@
-
+import React from 'react'
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import ContactForm from './contact-form';
 
-const stripePromise = loadStripe('pk_live_51Jopl3CrD5CMQwqmJZRW4JfF2dUdrZzqmxAz479JLYfnLdo0wITUOoUXubSV29T79kyXMPIyCWdcc9LBQM6olpry008acQTQqB');
 
 
 function StripeParent(props) {
+
+  const [stripe, setStripe] = React.useState(null)
+
+  React.useEffect(() => {
+  const initializeStripe = async ()=> {
+    const stripePromise = await loadStripe('pk_live_51Jopl3CrD5CMQwqmJZRW4JfF2dUdrZzqmxAz479JLYfnLdo0wITUOoUXubSV29T79kyXMPIyCWdcc9LBQM6olpry008acQTQqB');
+      setStripe(stripePromise)
+  }
+  initializeStripe()
+  }, [])
+
   return (
     <Elements stripe={stripePromise}>
       <ContactForm title={props.title} /> 
